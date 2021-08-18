50e5501966a2a8768d8b51ab548d79011900ed90
hangilc <hangil@chang.jp>
2021-08-18 21:04:01 +0900

    added create appoint times

--- db/src/main/scala/Db.scala
+++ db/src/main/scala/Db.scala
@@ -3,7 +3,10 @@ package dev.myclinic.scala.db
 import java.time.{LocalDate, LocalTime}
 import dev.myclinic.scala.db.DbSqlite
 import dev.myclinic.scala.db.DbPrim
+import dev.myclinic.scala.model._
 import cats.effect.IO
+import cats._
+import cats.implicits._
 import doobie._
 import doobie.implicits._
 import dev.myclinic.scala.model._
@@ -11,11 +14,29 @@ import dev.myclinic.scala.model._
 object Db {
   val xa = DbSqlite.xa
 
-  def exec[A](sql: ConnectionIO[A]): IO[A] = {
+  def query[A](sql: ConnectionIO[A]): IO[A] = {
     sql.transact(xa)
   }
 
   def listAppoint(from: LocalDate, upto: LocalDate): IO[List[Appoint]] = {
-    exec(DbPrim.listAppoint(from, upto))
+    query(DbPrim.listAppoint(from, upto))
+  }
+
+  def getAppoint(date: LocalDate, time: LocalTime): IO[Appoint] = {
+    query(DbPrim.getAppoint(date, time))
+  }
+
+  def findAppoint(date: LocalDate, time: LocalTime): IO[Option[Appoint]] = {
+    query(DbPrim.findAppoint(date, time))
+  }
+
+  def createAppointTimes(times: List[(LocalDate, LocalTime)]): IO[Unit] = {
+    def create(date: LocalDate, time: LocalTime): Appoint = 
+      Appoint(date, time, "", None, "")
+    
+    val ins = times.map(_ match {
+      case (d, t) => DbPrim.enterAppoint(create(d, t))
+    })
+    ins.traverse(query).map(_ => ())
   }
 }
--- db/src/main/scala/DbPrim.scala
+++ db/src/main/scala/DbPrim.scala
@@ -11,4 +11,20 @@ object DbPrim {
     sql"select * from appoint where date >= $from and date <= $upto order by date, time"
       .query[Appoint].to[List]
   }
+
+  def getAppoint(date: LocalDate, time: LocalTime): ConnectionIO[Appoint] = {
+    sql"select * from appint where date = $date and time = $time"
+      .query[Appoint].unique
+  }
+
+  def findAppoint(date: LocalDate, time: LocalTime): ConnectionIO[Option[Appoint]] = {
+    sql"select * from appint where date = $date and time = $time"
+      .query[Appoint].option
+  }
+
+  def enterAppoint(appoint: Appoint): ConnectionIO[Int] = {
+    val patientId: Int = appoint.patientId.getOrElse(0)
+    sql"""insert into appoint (date, time, patient_name, patient_id, memo) values 
+      (${appoint.date}, ${appoint.time}, ${appoint.patientName}, $patientId, ${appoint.memo})""".update.run
+  }
 }
