c156c31a24226dde89762a1d238cc444f417d73f
Hangil Chang <hangil@chang.jp>
2021-09-28 17:27:58 +0900

    adding ClinicOperation

--- util/src/main/scala/ClinicOperation.scala
+++ util/src/main/scala/ClinicOperation.scala
@@ -5,13 +5,22 @@ import java.time.DayOfWeek.*
 
 sealed trait ClinicOperation
 case object InOperation extends ClinicOperation
-case object RegularOutOfOperation extends ClinicOperation
-case class ClinicHoliday(name: String) extends ClinicOperation
+case object RegularHoliday extends ClinicOperation
+case class AdHocHoliday(name: String) extends ClinicOperation
 case class NationalHoliday(name: String) extends ClinicOperation
 
 object ClinicOperation:
-  def getClinicOperationAt(date: LocalDate): ClinicOperation = 
+  extension [A, B >: A](opt: Option[A])
+    def ||>(other: Option[B]): Option[B] = opt.orElse(other)
+
+  def getClinicOperationAt(date: LocalDate): ClinicOperation =
     import ClinicOperation.*
-    val dow = date.getDayOfWeek
-    if dow == SUNDAY || dow == WEDNESDAY then RegularOutOfOperation
-    else InOperation
+    def isAdHocWorkday: Option[ClinicOperation] = None
+    def isAdHocHoliday: Option[ClinicOperation] = None
+    def isRegularHoliday: Option[ClinicOperation] =
+      val dow = date.getDayOfWeek
+      if dow == SUNDAY || dow == WEDNESDAY then Some(RegularHoliday)
+      else None
+    (isAdHocWorkday ||> isAdHocHoliday ||> isRegularHoliday).getOrElse(
+      InOperation
+    )
