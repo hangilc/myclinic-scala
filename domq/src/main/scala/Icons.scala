15b08a85e6b89d68e10545bad5e5abcd781a2bd5
Hangil Chang <hangil@chang.jp>
2021-10-29 15:40:49 +0900

    added menu icons

--- domq/src/main/scala/Icons.scala
+++ domq/src/main/scala/Icons.scala
@@ -98,6 +98,34 @@ object Icons:
   // Based from Heroicons
   // src: https://github.com/tailwindlabs/heroicons
   // <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  //   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+  // </svg>
+  def menu(size: String = "1.5rem", color: String = "black"): HTMLElement =
+    makeIcon(
+      size,
+      color,
+      List(
+        "M4 6h16M4 12h16M4 18h16"
+      )
+    )
+
+  // Based from Heroicons
+  // src: https://github.com/tailwindlabs/heroicons
+  // <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  //   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8h16M4 16h16" />
+  // </svg>
+  def menuAlt4(size: String = "1.5rem", color: String = "black"): HTMLElement =
+    makeIcon(
+      size,
+      color,
+      List(
+        "M4 8h16M4 16h16"
+      )
+    )
+
+  // Based from Heroicons
+  // src: https://github.com/tailwindlabs/heroicons
+  // <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   //   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
   // </svg>
   def pencil(size: String = "1.5rem", color: String = "black"): HTMLElement =
