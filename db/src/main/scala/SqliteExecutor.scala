ed83bb77f8dca2436cd938f357718ec7244065f2
hangilc <hangil@chang.jp>
2021-10-31 10:15:57 +0900

    revised sqlite transaction mode

--- db/src/main/scala/SqliteExecutor.scala
+++ db/src/main/scala/SqliteExecutor.scala
@@ -8,6 +8,8 @@ import doobie.Transactor
 import doobie.implicits._
 import javax.sql.DataSource
 import cats.effect.IO
+import java.sql.Connection
+import java.sql.DriverManager
 
 object SqliteExecutor:
   val dbfile: String = sys.env.get("MYCLINIC_SQLITE_DB").get
@@ -17,19 +19,17 @@ object SqliteExecutor:
   config.setEncoding(SQLiteConfig.Encoding.UTF8)
   config.enforceForeignKeys(true)
   config.setBusyTimeout(60000)
+  config.setTransactionMode(SQLiteConfig.TransactionMode.IMMEDIATE)
   val ds = new SQLiteDataSource(config)
   ds.setUrl(url)
 
-  def transactor(ds: DataSource) =
-    for
-      ce <- ExecutionContexts.fixedThreadPool[IO](32)
-    yield Transactor.fromDataSource[IO](ds, ce)
-
-  val xa = transactor(ds)
+  val ec = ExecutionContexts.fixedThreadPool[IO](6)
 
   def execute[A](op: ConnectionIO[A]): IO[A] =
-    xa.use(tx => op.transact(tx))
-
+    ec.use(c => {
+      val xa = Transactor.fromDataSource[IO](ds, c)
+      op.transact(xa)
+    })
 
 trait Sqlite {
   def sqlite[A](op: ConnectionIO[A]): IO[A] = SqliteExecutor.execute(op)
