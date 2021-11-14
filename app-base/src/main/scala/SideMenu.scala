1ba4928b8811a6e32db70d56e65495e8bb7cb252
hangilc <hangil@chang.jp>
2021-11-15 00:07:24 +0900

    added SideMenu

---
+++ app-base/src/main/scala/SideMenu.scala
@@ -0,0 +1,68 @@
+package dev.myclinic.scala.web.appbase
+
+import dev.fujiwara.domq.ElementQ.{*, given}
+import dev.fujiwara.domq.Html.{*, given}
+import dev.fujiwara.domq.Modifiers.{*, given}
+import dev.fujiwara.domq.{ShowMessage}
+import scala.language.implicitConversions
+import org.scalajs.dom.raw.{HTMLElement}
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.Success
+import scala.util.Failure
+
+case class SideMenuItem(label: String, creator: () => HTMLElement)
+
+class SideMenu(args: List[(String, () => Future[HTMLElement])]):
+  val ele: HTMLElement = div()
+
+  private case class Item(
+      label: String,
+      link: HTMLElement,
+      creator: () => Future[HTMLElement],
+      var cache: Option[HTMLElement]
+  ):
+    def inactivate(): Unit =
+      cache.foreach(e => {
+        e.remove()
+        link(cls :- "current")
+      })
+    def activate(): Future[Unit] =
+      cache.fold({
+        for
+          e <- creator()
+        yield {
+          cache = Some(e)
+          ele(e)
+          link(cls := "current")
+          ()
+        }
+      })(e => {
+        ele(e)
+        link(cls := "current")
+        Future.successful(())
+      })
+
+
+  private val items: List[Item] = args.map(arg => arg match {
+    case (label, creator) => {
+      val link = a(label)
+      val item = Item(label, link, creator, None)
+      link(onclick := (() => invoke(item)))
+      item
+    }
+  })
+  items.foreach(item => {
+    ele(item.link)
+  })
+  private var current: Option[Item] = None
+
+  private def invoke(item: Item): Unit =
+    current.foreach(item => {
+      item.inactivate()
+      current = None
+    })
+    item.activate().onComplete {
+      case Success(_) => current = Some(item)
+      case Failure(ex) => ShowMessage.showError(ex.getMessage)
+    }
