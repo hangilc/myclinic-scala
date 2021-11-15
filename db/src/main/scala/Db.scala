7d8152aa9107db637e27477c10ab02c738a72c76
hangilc <hangil@chang.jp>
2021-11-15 21:41:08 +0900

    added count*ForVisit

--- db/src/main/scala/Db.scala
+++ db/src/main/scala/Db.scala
@@ -1,4 +1,16 @@
 package dev.myclinic.scala.db
 
-object Db extends Mysql with DbAppoint with DbEvent with DbPatient 
-  with DbHotline with DbWqueue with DbVisit
+object Db
+    extends Mysql
+    with DbAppoint
+    with DbEvent
+    with DbPatient
+    with DbHotline
+    with DbWqueue
+    with DbVisit
+    with DbText
+    with DbDrug
+    with DbShinryou
+    with DbConduct
+    with DbCharge
+    with DbPayment
---
+++ db/src/main/scala/DbDrug.scala
@@ -0,0 +1,20 @@
+package dev.myclinic.scala.db
+
+import cats.*
+import cats.implicits.*
+import cats.effect.IO
+import dev.myclinic.scala.model.*
+import dev.myclinic.scala.db.{DbPatientPrim => Prim}
+import doobie.*
+import doobie.implicits.*
+
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.temporal.ChronoUnit
+import java.util.regex.Pattern
+
+trait DbDrug extends Mysql:
+  def countDrugForVisit(visitId: Int): IO[Int] =
+    mysql(sql"""
+      select count(*) from visit_drug where visit_id = ${visitId}
+    """.query[Int].unique)
