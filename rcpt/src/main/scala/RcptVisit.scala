e0a6d9042da968f9d7e307c69a7f9a09de528de5
Hangil Chang <hangil@chang.jp>
2021-11-20 16:44:15 +0900

    adding getMeisai

--- rcpt/src/main/scala/RcptVisit.scala
+++ rcpt/src/main/scala/RcptVisit.scala
@@ -10,8 +10,8 @@ object RcptVisit:
   ): List[(MeisaiSection, List[MeisaiSectionItem])] =
     val items: ListBuffer[MeisaiUnit] = ListBuffer()
     if !visit.drugs.isEmpty then new RuntimeException("visit drug is not supported")
-    visit.shinryouList.foreach(s => items :+ MeisaiUnit.fromShinryou(s))
-    visit.conducts.foreach(c => items :+ MeisaiUnit.fromConduct(c))
+    visit.shinryouList.foreach(s => items.addOne(MeisaiUnit.fromShinryou(s)))
+    visit.conducts.foreach(c => items.addOne(MeisaiUnit.fromConduct(c)))
     items.toList.groupBy(_.section).toList.sortBy(_._1.ordinal)
     .map({
       case (sect, units) => (sect, units.map(_.toItem))
