d536b79e9de375666280b9ddbd252b11f55e96e2
hangilc <hangil@chang.jp>
2021-12-22 17:51:14 +0900

    refactored savePatientImage

--- server/src/main/scala/FileService.scala
+++ server/src/main/scala/FileService.scala
@@ -16,24 +16,27 @@ import fs2.concurrent.Topic
 import org.http4s.websocket.WebSocketFrame
 import fs2.Chunk
 import scodec.bits.ByteVector
-import java.nio.file.Paths
+import fs2.io.file.Path
 
 object FileService extends DateTimeQueryParam with Publisher:
   object intPatientId extends QueryParamDecoderMatcher[Int]("patient-id")
   object strFileName extends QueryParamDecoderMatcher[String]("file-name")
 
+  private def saveToFile(req: Request[IO], path: Path): IO[Response[IO]] =
+    req.body
+      .through(fs2.io.file.Files[IO].writeAll(path))
+      .compile
+      .drain
+      .map(_ =>
+        Response[IO]()
+          .withStatus(Status.Ok)
+          .withEntity[Boolean](true)
+          .withContentType(`Content-Type`(MediaType.application.json))
+      )
+
   def routes(using topic: Topic[IO, WebSocketFrame]) = HttpRoutes.of[IO] {
     case req @ POST -> Root / "save-patient-image" :? intPatientId(
           patientId
         ) +& strFileName(fileName) =>
-      req.body
-        .through(fs2.io.file.writeAll(Paths.get("../image.jpg")))
-        .compile
-        .drain
-        .map(_ =>
-          Response[IO]()
-            .withStatus(Status.Ok)
-            .withEntity[Boolean](true)
-            .withContentType(`Content-Type`(MediaType.application.json))
-        )
+      saveToFile(req, Path("../image.jpg"))
   }
