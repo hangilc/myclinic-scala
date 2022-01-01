bde562ee7579083b31e2a8f1b4593c346d1fae4e
hangilc <hangil@chang.jp>
2022-01-02 01:18:59 +0900

    temporary saving

--- domq/src/main/scala/Elements.scala
+++ domq/src/main/scala/Elements.scala
@@ -8,20 +8,19 @@ import org.scalajs.dom.document
 import math.Ordered.orderingToOrdered
 import dev.fujiwara.domq.CustomEvent
 
-trait Modifier[+E]:
-  def modify[E2 >: E <: HTMLElement](e: E2): Unit
+trait Modifier[-E]:
+  def modify(e: E): Unit
 
 object Modifier:
-  private def assignModifier[E] 
   object cls:
-    def :=[E](arg: String): Modifier[E] = new Modifier[E]:
-      def modify[E2 >: E <: HTMLElement](e: E2): Unit = e.className = arg
+    def :=[E <: HTMLElement](arg: String): Modifier[E] = new Modifier[E]:
+      def modify(e: E): Unit = e.className = arg
 
   private lazy val textAreaWorkarea = document.createElement("textarea")
 
-  def raw[E](text: String): Modifier[E] =
+  def raw[E <: HTMLElement](text: String): Modifier[E] =
     new Modifier[E]:
-      def modify[E2 >: E <: HTMLElement](e: E2): Unit =
+      def modify(e: E): Unit =
         textAreaWorkarea.innerHTML = text
         val decoded = textAreaWorkarea.innerText
         e.appendChild(document.createTextNode(decoded))
