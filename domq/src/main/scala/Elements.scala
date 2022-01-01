398de419fc95564f999c0246265e41474b6d974f
hangilc <hangil@chang.jp>
2022-01-02 01:09:02 +0900

    temporary saving

--- domq/src/main/scala/Elements.scala
+++ domq/src/main/scala/Elements.scala
@@ -1,17 +1,27 @@
-package dev.fujiwara.domq.rev
+package dev.fujiwara.domq
 
 import org.scalajs.dom.{HTMLElement, HTMLSelectElement}
 import scala.collection.mutable.ListBuffer
 import org.scalajs.dom.HTMLOptionElement
 import org.scalajs.dom.HTMLInputElement
+import org.scalajs.dom.document
 import math.Ordered.orderingToOrdered
 import dev.fujiwara.domq.CustomEvent
 
-trait ElementModifier[+E]:
+trait Modifier[+E]:
   def modify[E2 >: E <: HTMLElement](e: E2): Unit
 
-object ElementModifier:
+object Modifier:
+  private def assignModifier[E] 
   object cls:
-    def :=[E](arg: String): ElementModifier[E] = new ElementModifier[E]:
+    def :=[E](arg: String): Modifier[E] = new Modifier[E]:
       def modify[E2 >: E <: HTMLElement](e: E2): Unit = e.className = arg
 
+  private lazy val textAreaWorkarea = document.createElement("textarea")
+
+  def raw[E](text: String): Modifier[E] =
+    new Modifier[E]:
+      def modify[E2 >: E <: HTMLElement](e: E2): Unit =
+        textAreaWorkarea.innerHTML = text
+        val decoded = textAreaWorkarea.innerText
+        e.appendChild(document.createTextNode(decoded))
--- domq/src/main/scala/Modifier.scala
+++ domq/src/main/scala/Modifier.scala
@@ -1,260 +1,260 @@
-package dev.fujiwara.domq
+// package dev.fujiwara.domq
 
-import org.scalajs.dom.document
-import org.scalajs.dom.HTMLElement
-import org.scalajs.dom.MouseEvent
-import scala.language.implicitConversions
-import org.scalajs.dom.CSSStyleDeclaration
-import scala.scalajs.js
-import org.scalajs.dom.Event
-import org.scalajs.dom.HTMLInputElement
-import org.scalajs.dom.KeyboardEvent
+// import org.scalajs.dom.document
+// import org.scalajs.dom.HTMLElement
+// import org.scalajs.dom.MouseEvent
+// import scala.language.implicitConversions
+// import org.scalajs.dom.CSSStyleDeclaration
+// import scala.scalajs.js
+// import org.scalajs.dom.Event
+// import org.scalajs.dom.HTMLInputElement
+// import org.scalajs.dom.KeyboardEvent
 
-trait Modifier[+E]:
-  def modify[E2 >: E <: HTMLElement](e: E2): Unit
+// trait Modifier[+E]:
+//   def modify[E2 >: E <: HTMLElement](e: E2): Unit
 
-object ModifierOrig:
+// object ModifierOrig:
 
-  given toTextModifier: scala.Conversion[String, Modifier] = data =>
-    Modifier(e => {
-      val t = document.createTextNode(data)
-      e.appendChild(t)
-    })
+//   given toTextModifier: scala.Conversion[String, Modifier] = data =>
+//     Modifier(e => {
+//       val t = document.createTextNode(data)
+//       e.appendChild(t)
+//     })
 
-  implicit def toChildModifier(e: HTMLElement): Modifier =
-    Modifier(ele => {
-      ele.appendChild(e)
-    })
+//   implicit def toChildModifier(e: HTMLElement): Modifier =
+//     Modifier(ele => {
+//       ele.appendChild(e)
+//     })
 
-  implicit def toChildModifier(e: ElementQ): Modifier =
-    Modifier(ele => {
-      ele.appendChild(e.ele)
-    })
+//   implicit def toChildModifier(e: ElementQ): Modifier =
+//     Modifier(ele => {
+//       ele.appendChild(e.ele)
+//     })
 
-  implicit def toListModifier(ms: List[Modifier]): Modifier =
-    Modifier(target => {
-      ms.foreach(_.modifier(target))
-    })
+//   implicit def toListModifier(ms: List[Modifier]): Modifier =
+//     Modifier(target => {
+//       ms.foreach(_.modifier(target))
+//     })
 
-  private val ta = document.createElement("textarea")
+//   private val ta = document.createElement("textarea")
 
-  def raw(text: String): Modifier = Modifier(target => {
-    ta.innerHTML = text
-    val decoded = ta.innerText
-    target.appendChild(document.createTextNode(decoded))
-  })
+//   def raw(text: String): Modifier = Modifier(target => {
+//     ta.innerHTML = text
+//     val decoded = ta.innerText
+//     target.appendChild(document.createTextNode(decoded))
+//   })
 
-object Modifiers:
+// object Modifiers:
 
-  case class Creator[A](f: (HTMLElement, A) => Unit):
-    def :=(arg: A) = Modifier(e => f(e, arg))
+//   case class Creator[A](f: (HTMLElement, A) => Unit):
+//     def :=(arg: A) = Modifier(e => f(e, arg))
 
-  case class ClsModifier():
-    def :=(arg: String) = Modifier(e => {
-      for c <- arg.split("\\s+") do e.classList.add(c)
-    })
+//   case class ClsModifier():
+//     def :=(arg: String) = Modifier(e => {
+//       for c <- arg.split("\\s+") do e.classList.add(c)
+//     })
 
-    def :-(arg: String) = Modifier(e => {
-      for c <- arg.split("\\s+") do e.classList.remove(c)
-    })
+//     def :-(arg: String) = Modifier(e => {
+//       for c <- arg.split("\\s+") do e.classList.remove(c)
+//     })
 
-  val cls = ClsModifier()
+//   val cls = ClsModifier()
 
-  val cb = Creator[HTMLElement => Unit]((e, handler) => handler(e))
+//   val cb = Creator[HTMLElement => Unit]((e, handler) => handler(e))
 
-  case class Attr(name: String):
-    def :=(arg: String): Modifier = Modifier(e => e.setAttribute(name, arg))
-    def remove: Modifier = Modifier(e => e.removeAttribute(name))
+//   case class Attr(name: String):
+//     def :=(arg: String): Modifier = Modifier(e => e.setAttribute(name, arg))
+//     def remove: Modifier = Modifier(e => e.removeAttribute(name))
 
-  def attr(name: String): Attr = Attr(name)
+//   def attr(name: String): Attr = Attr(name)
 
-  object value:
-    def :=(arg: String) = Modifier(e => e.asInstanceOf[HTMLInputElement].value = arg)
+//   object value:
+//     def :=(arg: String) = Modifier(e => e.asInstanceOf[HTMLInputElement].value = arg)
 
-  val id = attr("id")
+//   val id = attr("id")
 
-  def attrNS(namespace: String, name: String) = Creator[String]((e, a) => {
-    e.setAttributeNS(namespace, name, a)
-  })
+//   def attrNS(namespace: String, name: String) = Creator[String]((e, a) => {
+//     e.setAttributeNS(namespace, name, a)
+//   })
 
-  def placeholder: Attr = attr("placeholder")
+//   def placeholder: Attr = attr("placeholder")
 
-  def disabled: Creator[Boolean] = Creator[Boolean]((e, disable) => {
-    if disable then e.setAttribute("disabled", "disabled")
-    else e.removeAttribute("disabled")
-  })
+//   def disabled: Creator[Boolean] = Creator[Boolean]((e, disable) => {
+//     if disable then e.setAttribute("disabled", "disabled")
+//     else e.removeAttribute("disabled")
+//   })
 
-  def checked: Creator[Boolean] = Creator[Boolean]((e, check) => {
-    if e.isInstanceOf[HTMLInputElement] then
-      val eCheck = e.asInstanceOf[HTMLInputElement]
-      eCheck.checked = check
-  })
+//   def checked: Creator[Boolean] = Creator[Boolean]((e, check) => {
+//     if e.isInstanceOf[HTMLInputElement] then
+//       val eCheck = e.asInstanceOf[HTMLInputElement]
+//       eCheck.checked = check
+//   })
 
-  val name: Attr = attr("name")
+//   val name: Attr = attr("name")
 
-  def css(f: CSSStyleDeclaration => Unit): Modifier = Modifier(e => f(e.style))
+//   def css(f: CSSStyleDeclaration => Unit): Modifier = Modifier(e => f(e.style))
 
-  val mt = Creator[String]((e, value) => e.style.marginTop = value)
-  val mb = Creator[String]((e, value) => e.style.marginBottom = value)
-  val ml = Creator[String]((e, value) => e.style.marginLeft = value)
-  val mr = Creator[String]((e, value) => e.style.marginRight = value)
+//   val mt = Creator[String]((e, value) => e.style.marginTop = value)
+//   val mb = Creator[String]((e, value) => e.style.marginBottom = value)
+//   val ml = Creator[String]((e, value) => e.style.marginLeft = value)
+//   val mr = Creator[String]((e, value) => e.style.marginRight = value)
 
-  val leftGap: Modifier = ml := "0.5rem"
-  val topGap: Modifier = mt := "0.5em"
+//   val leftGap: Modifier = ml := "0.5rem"
+//   val topGap: Modifier = mt := "0.5em"
 
-  private def styleSetter(
-      f: (CSSStyleDeclaration, String) => Unit
-  ): Creator[String] =
-    Creator[String]((e, a) => f(e.style, a))
+//   private def styleSetter(
+//       f: (CSSStyleDeclaration, String) => Unit
+//   ): Creator[String] =
+//     Creator[String]((e, a) => f(e.style, a))
 
-  val padding = styleSetter((s, v) => s.padding = v)
-  val margin = styleSetter((s, v) => s.margin = v)
-  val border = styleSetter((s, v) => s.border = v)
-  val color = styleSetter((s, v) => s.color = v)
-  val background = styleSetter((s, v) => s.background = v)
-  val cursor = styleSetter((s, v) => s.cursor = v)
-  val display = styleSetter((style, value) => style.display = value)
-  val displayNone = display := "none"
-  val displayBlock = display := "block"
-  val displayInlineBlock = display := "inline-block"
-  val displayDefault = display := ""
-  val justifyContent =
-    styleSetter((style, value) => style.setProperty("justify-content", value))
-  val alignItems =
-    styleSetter((style, value) => style.setProperty("align-items", value))
-  val maxHeight = styleSetter((s, v) => s.maxHeight = v)
-  val minHeight = styleSetter((s, v) => s.minHeight = v)
-  val width = styleSetter((s, v) => if !v.isEmpty then s.width = v)
-  val height = styleSetter((s, v) => s.height = v)
-  val overflowY = styleSetter((s, v) => s.overflowY = v)
-  val overflowYAuto = overflowY := "auto"
-  val cssFloat = styleSetter((s, v) => s.cssFloat = v)
-  val floatRight = cssFloat := "right"
-  val textAlign = styleSetter((s, v) => s.textAlign = v)
-  val stroke = styleSetter((s, v) => s.stroke = v)
-  val zIndex = Creator[Int]((e, v) => e.style.zIndex = v.toString)
+//   val padding = styleSetter((s, v) => s.padding = v)
+//   val margin = styleSetter((s, v) => s.margin = v)
+//   val border = styleSetter((s, v) => s.border = v)
+//   val color = styleSetter((s, v) => s.color = v)
+//   val background = styleSetter((s, v) => s.background = v)
+//   val cursor = styleSetter((s, v) => s.cursor = v)
+//   val display = styleSetter((style, value) => style.display = value)
+//   val displayNone = display := "none"
+//   val displayBlock = display := "block"
+//   val displayInlineBlock = display := "inline-block"
+//   val displayDefault = display := ""
+//   val justifyContent =
+//     styleSetter((style, value) => style.setProperty("justify-content", value))
+//   val alignItems =
+//     styleSetter((style, value) => style.setProperty("align-items", value))
+//   val maxHeight = styleSetter((s, v) => s.maxHeight = v)
+//   val minHeight = styleSetter((s, v) => s.minHeight = v)
+//   val width = styleSetter((s, v) => if !v.isEmpty then s.width = v)
+//   val height = styleSetter((s, v) => s.height = v)
+//   val overflowY = styleSetter((s, v) => s.overflowY = v)
+//   val overflowYAuto = overflowY := "auto"
+//   val cssFloat = styleSetter((s, v) => s.cssFloat = v)
+//   val floatRight = cssFloat := "right"
+//   val textAlign = styleSetter((s, v) => s.textAlign = v)
+//   val stroke = styleSetter((s, v) => s.stroke = v)
+//   val zIndex = Creator[Int]((e, v) => e.style.zIndex = v.toString)
 
-  val showHide = Creator[Boolean]((e, show: Boolean) => {
-    val q = ElementQ(e)
-    if show then q(displayDefault) else q(displayNone)
-  })
+//   val showHide = Creator[Boolean]((e, show: Boolean) => {
+//     val q = ElementQ(e)
+//     if show then q(displayDefault) else q(displayNone)
+//   })
 
-  val adjustForFlex = Modifier(e => {
-    e.setAttribute("size", "1")
-    e.style.setProperty("flex-base", "0")
-  })
-  val flexGrow = styleSetter((s, v) => s.setProperty("flex-grow", v))
-  val flexShrink = styleSetter((s, v) => s.setProperty("flex-shrink", v))
-  val flex = styleSetter((s, v) => s.setProperty("flex", v))
+//   val adjustForFlex = Modifier(e => {
+//     e.setAttribute("size", "1")
+//     e.style.setProperty("flex-base", "0")
+//   })
+//   val flexGrow = styleSetter((s, v) => s.setProperty("flex-grow", v))
+//   val flexShrink = styleSetter((s, v) => s.setProperty("flex-shrink", v))
+//   val flex = styleSetter((s, v) => s.setProperty("flex", v))
 
-  def hoverBackground(bg: String): Modifier =
-    var save: String = ""
-    Modifier(e => {
-      ElementQ(e)(
-        onmouseenter := (() => {
-          save = e.style.background
-          e.style.background = bg
-        }),
-        onmouseleave := (() => {
-          e.style.background = save
-        })
-      )
-    })
+//   def hoverBackground(bg: String): Modifier =
+//     var save: String = ""
+//     Modifier(e => {
+//       ElementQ(e)(
+//         onmouseenter := (() => {
+//           save = e.style.background
+//           e.style.background = bg
+//         }),
+//         onmouseleave := (() => {
+//           e.style.background = save
+//         })
+//       )
+//     })
 
-  val innerText = Creator[String]((e, t) => e.innerText = t)
+//   val innerText = Creator[String]((e, t) => e.innerText = t)
 
-  val href = Creator[String]((e, a) => {
-    val value = if a.isEmpty then "javascript:void(0)" else a
-    e.setAttribute("href", value)
-  })
+//   val href = Creator[String]((e, a) => {
+//     val value = if a.isEmpty then "javascript:void(0)" else a
+//     e.setAttribute("href", value)
+//   })
 
-  object onclick:
-    def :=(f: MouseEvent => Any) = Modifier(e => {
-      e.addEventListener("click", f)
-    })
+//   object onclick:
+//     def :=(f: MouseEvent => Any) = Modifier(e => {
+//       e.addEventListener("click", f)
+//     })
 
-    def :=(f: () => Any) = Modifier(e => {
-      e.addEventListener("click", (_: MouseEvent) => f())
-    })
+//     def :=(f: () => Any) = Modifier(e => {
+//       e.addEventListener("click", (_: MouseEvent) => f())
+//     })
 
-  object onsubmit:
-    def :=(f: () => Unit) = Modifier(e => {
-      e.addEventListener(
-        "submit",
-        (e: Event) => {
-          e.preventDefault
-          e.stopPropagation
-          f()
-        }
-      )
-    })
+//   object onsubmit:
+//     def :=(f: () => Unit) = Modifier(e => {
+//       e.addEventListener(
+//         "submit",
+//         (e: Event) => {
+//           e.preventDefault
+//           e.stopPropagation
+//           f()
+//         }
+//       )
+//     })
 
-  object onmousedown:
-    def :=(f: MouseEvent => Unit) = Modifier(e => {
-      e.addEventListener("mousedown", f)
-    })
+//   object onmousedown:
+//     def :=(f: MouseEvent => Unit) = Modifier(e => {
+//       e.addEventListener("mousedown", f)
+//     })
 
-  object onmouseup:
-    def :=(f: MouseEvent => Unit) = Modifier(e => {
-      e.addEventListener("mouseup", f)
-    })
+//   object onmouseup:
+//     def :=(f: MouseEvent => Unit) = Modifier(e => {
+//       e.addEventListener("mouseup", f)
+//     })
 
-  object onmousemove:
-    def :=(f: js.Function1[MouseEvent, Unit]) = Modifier(e => {
-      if f == null then System.err.println("null handler for onmousemove")
-      e.addEventListener("mousemove", f)
-    })
+//   object onmousemove:
+//     def :=(f: js.Function1[MouseEvent, Unit]) = Modifier(e => {
+//       if f == null then System.err.println("null handler for onmousemove")
+//       e.addEventListener("mousemove", f)
+//     })
 
-    def :-(f: js.Function1[MouseEvent, Unit]) = Modifier(e => {
-      if f == null then System.err.println("null handler for onmousemove")
-      e.removeEventListener("mousemove", f)
-    })
+//     def :-(f: js.Function1[MouseEvent, Unit]) = Modifier(e => {
+//       if f == null then System.err.println("null handler for onmousemove")
+//       e.removeEventListener("mousemove", f)
+//     })
 
-  object onmouseenter:
-    def :=(f: MouseEvent => Unit) = Modifier(e => {
-      if f == null then System.err.println("null handler for onmouseenter")
-      e.addEventListener("mouseenter", f)
-    })
+//   object onmouseenter:
+//     def :=(f: MouseEvent => Unit) = Modifier(e => {
+//       if f == null then System.err.println("null handler for onmouseenter")
+//       e.addEventListener("mouseenter", f)
+//     })
 
-    def :=(f: () => Unit) = Modifier(e => {
-      e.addEventListener("mouseenter", (_: MouseEvent) => f())
-    })
+//     def :=(f: () => Unit) = Modifier(e => {
+//       e.addEventListener("mouseenter", (_: MouseEvent) => f())
+//     })
 
-  object onmouseleave:
-    def :=(f: js.Function1[MouseEvent, Unit]) = Modifier(e => {
-      if f == null then System.err.println("null handler for onmouseleave")
-      e.addEventListener("mouseleave", f)
-    })
+//   object onmouseleave:
+//     def :=(f: js.Function1[MouseEvent, Unit]) = Modifier(e => {
+//       if f == null then System.err.println("null handler for onmouseleave")
+//       e.addEventListener("mouseleave", f)
+//     })
 
-    def :-(f: js.Function1[MouseEvent, Unit]) = Modifier(e => {
-      if f == null then System.err.println("null handler for onmouseleave")
-      e.removeEventListener("mouseleave", f)
-    })
+//     def :-(f: js.Function1[MouseEvent, Unit]) = Modifier(e => {
+//       if f == null then System.err.println("null handler for onmouseleave")
+//       e.removeEventListener("mouseleave", f)
+//     })
 
-    def :=(f: () => Unit) = Modifier(e => {
-      e.addEventListener("mouseleave", (_: MouseEvent) => f())
-    })
+//     def :=(f: () => Unit) = Modifier(e => {
+//       e.addEventListener("mouseleave", (_: MouseEvent) => f())
+//     })
 
-  val oncontextmenu =
-    Creator[js.Function1[MouseEvent, Unit]]((ele, handler) => {
-      if handler == null then
-        System.err.println("null handler for oncontextmenu")
-      ele.addEventListener("contextmenu", handler)
-    })
+//   val oncontextmenu =
+//     Creator[js.Function1[MouseEvent, Unit]]((ele, handler) => {
+//       if handler == null then
+//         System.err.println("null handler for oncontextmenu")
+//       ele.addEventListener("contextmenu", handler)
+//     })
 
-  object onchange:
-    def :=(handler: js.Function1[Event, Unit]) = Modifier(ele => {
-      if handler == null then System.err.println("null handler for onchange")
-      ele.addEventListener("change", handler)
-    })
-    def :=(handler: () => Unit) =
-      Modifier(ele => ele.addEventListener("change", (_: Event) => handler()))
+//   object onchange:
+//     def :=(handler: js.Function1[Event, Unit]) = Modifier(ele => {
+//       if handler == null then System.err.println("null handler for onchange")
+//       ele.addEventListener("change", handler)
+//     })
+//     def :=(handler: () => Unit) =
+//       Modifier(ele => ele.addEventListener("change", (_: Event) => handler()))
 
-  object onkeyup:
-    def :=(handler: KeyboardEvent => Unit): Modifier =
-      Modifier(ele => ele.addEventListener("keyup", handler))
+//   object onkeyup:
+//     def :=(handler: KeyboardEvent => Unit): Modifier =
+//       Modifier(ele => ele.addEventListener("keyup", handler))
 
-  class CustomEventModifier[T](eventType: String):
-    def :=(f: CustomEvent[T] => Any) = Modifier(e => e.addEventListener(eventType, f))
+//   class CustomEventModifier[T](eventType: String):
+//     def :=(f: CustomEvent[T] => Any) = Modifier(e => e.addEventListener(eventType, f))
 
-  def oncustomevent[T](eventType: String) = new CustomEventModifier[T](eventType)
+//   def oncustomevent[T](eventType: String) = new CustomEventModifier[T](eventType)
