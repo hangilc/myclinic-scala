38935d5e4633d2e19cfd6eeed9e286c681003874
hangilc <hangil@chang.jp>
2022-01-31 23:32:26 +0900

    added ui of make appoint dialog

--- appoint-app/src/main/scala/sheet/appointdialog/MakeAppointDialog.scala
+++ appoint-app/src/main/scala/sheet/appointdialog/MakeAppointDialog.scala
@@ -17,6 +17,8 @@ class MakeAppointDialog(
     ui.commands
   )
   ui.cancelButton(onclick := (() => dlog.close()))
+  ui.nameValue.onSelect = patient =>
+    ui.patientIdDisp(innerText := patient.patientId.toString)
 
   def open(): Unit =
     dlog.open()
@@ -25,18 +27,36 @@ class MakeAppointDialog(
   def close(): Unit = dlog.close()
 
 object MakeAppointDialog:
-  def apply(appointTime: AppointTime, followingVacantRegular: () => Option[AppointTime]): MakeAppointDialog =
+  def apply(
+      appointTime: AppointTime,
+      followingVacantRegular: () => Option[AppointTime]
+  ): MakeAppointDialog =
     new MakeAppointDialog(new UI, appointTime, followingVacantRegular)
 
   class UI:
     val appointTimeDisp = div
     val nameValue = NameValue()
+    val patientIdDisp = span
+    val memoInput = inputText
+    val kenshinCheck = checkbox()
+    val alsoWrapper = span()
+    val alsoCheck = checkbox()
     val enterButton = button
     val cancelButton = button
     val body = div(
       appointTimeDisp,
       Form.rows(
-        span("患者名：") -> nameValue.ui.ele
+        span("患者名：") -> nameValue.ui.ele,
+        span("患者番号：") -> patientIdDisp,
+        span("メモ：") -> memoInput,
+        span("タグ：") -> div(
+          kenshinCheck,
+          "健診",
+          alsoWrapper(
+            alsoCheck(disabled := true),
+            label("診察も")
+          )
+        )
       )
     )
     val commands = div(
@@ -46,18 +66,24 @@ object MakeAppointDialog:
     def initFocus(): Unit = nameValue.initFocus()
 
   class NameValue(val ui: NameValue.UI):
+    var onSelect: Patient => Unit = _ => ()
     ui.searchForm(onsubmit := (onSubmit _))
     ui.searchIcon(onclick := (onSubmit _))
-    ui.searchResult.onSelect = ???
+    ui.searchResult.onSelect = patient =>
+      ui.input.value = patient.fullName()
+      ui.hideSearchResult()
+      onSelect(patient)
 
     def onSubmit(): Unit =
       val txt = value
       if !txt.isEmpty then
-        for
-          patients <- Api.searchPatient(txt)
-        yield 
-          ui.searchResult.setItems(patients, patient => s"(${patient.patientId}) ${patient.fullName()}")
-          ui.searchResult.show()
+        for patients <- Api.searchPatient(txt)
+        yield
+          ui.searchResult.setItems(
+            patients,
+            patient => s"(${patient.patientId}) ${patient.fullName()}"
+          )
+          ui.showSearchResult()
 
     def value: String = ui.input.value
     def initFocus(): Unit = ui.input.focus()
@@ -72,11 +98,18 @@ object MakeAppointDialog:
       val input = inputText(width := "100%")
       val searchIcon = Icons.search
       val searchResult = Selection[Patient]()
+      private val searchResultWrapper = div(displayNone)
       searchResult.hide()
       val ele = div(
         inputArea(Form.inputGroup)(
           searchForm(input, flex := "1 1 auto"),
           searchIcon(Icons.defaultStyle, ml := "0.5rem")
         ),
-        searchResult.ele
+        searchResultWrapper(searchResult.ele(mt := "6px"))
       )
+      def showSearchResult(): Unit =
+        searchResult.show()
+        searchResultWrapper(displayDefault)
+      def hideSearchResult(): Unit =
+        searchResult.hide()
+        searchResultWrapper(displayNone)
