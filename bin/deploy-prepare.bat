0f5703f9da82c22f3871d7e64587496e65ee7556
hangilc <hangil@chang.jp>
2022-02-02 23:27:34 +0900

    temporary modified deploy-prepare.bat

--- bin/deploy-prepare.bat
+++ bin/deploy-prepare.bat
@@ -1,5 +1,5 @@
 call sbt clean
 call sbt server/pack
 call sbt appointApp/fullLinkJS
-call sbt receptionApp/fullLinkJS
+REM call sbt receptionApp/fullLinkJS
 xcopy /E /I server\web server\target\pack\web\
