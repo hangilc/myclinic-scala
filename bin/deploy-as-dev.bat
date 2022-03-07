c586ad000e25042dcf30b5734b46dd57a5e92b62
hangilc <hangil@chang.jp>
2022-03-07 10:11:55 +0900

    renamed bin files

--- bin/deploy-as-dev.bat
+++
@@ -1,3 +0,0 @@
-@REM set TIMESTAMP=%date:~0,4%-%date:~5,2%%date:~8,2%-%time:~0,2%%time:~3,2%
-ssh changclinic-server rm -rf ~/myclinic-scala-server/dev
-scp -r server\target\pack changclinic-server:~/myclinic-scala-server/dev
--- bin/deploy-prepare.bat
+++
@@ -1,5 +0,0 @@
-call sbt clean
-call sbt server/pack
-call sbt appointApp/fullLinkJS
-REM call sbt receptionApp/fullLinkJS
-xcopy /E /I server\web server\target\pack\web\
