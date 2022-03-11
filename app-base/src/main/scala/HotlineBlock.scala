1926c325d4573fea9f6a2b585467f8d3960e9cd9
hangilc <hangil@chang.jp>
2022-03-11 15:45:31 +0900

    adding hotline block

--- app-base/src/main/scala/HotlineBlock.scala
+++ app-base/src/main/scala/HotlineBlock.scala
@@ -1,11 +1,39 @@
 package dev.myclinic.scala.web.appbase
 
 import dev.fujiwara.domq.all.{*, given}
+import scala.concurrent.Future
+import dev.myclinic.scala.webclient.{Api, global}
+import io.circe.parser.decode
+import dev.myclinic.scala.model.Hotline
+import dev.myclinic.scala.model.jsoncodec.Implicits.given
 
 class HotlineBlock:
   val ui = new HotlineBlockUI
 
   def ele = ui.ele
+  def init(): Future[Unit] =
+    for
+      hotlines <- Api.listTodaysHotline()
+    yield
+      hotlines.foreach(event => 
+        val appEventId = event.appEventId
+        val hotline = event.data
+        handleHotline(appEventId, hotline)
+      )
+      // fetcher.catchup(lastAppEventId, event =>
+      //   (event.model, event.kind) match {
+      //     case (Hotline.modelSymbol, AppModelEvent.createdSymbol) =>
+      //       handleHotline(event.appEventId, event.data.asInstanceOf[Hotline])
+      //     case _ => ()
+      //   }
+      // )
+      // ui.sendButton(onclick := (() => onSend(ui.messageInput.value.trim)))
+      // ui.rogerButton(onclick := (() => onSend("了解")))
+      // ui.beepButton(onclick := (() => { Api.hotlineBeep(sendTo); () }))
+  private def decodeHotline(data: String): Hotline =
+    decode[Hotline](data).toOption.get
+  private def handleHotline(gen: Int, message: String): Unit =
+    println(("hotline", message))
 
 class HotlineBlockUI:
   import dev.fujiwara.domq.PullDownLink
