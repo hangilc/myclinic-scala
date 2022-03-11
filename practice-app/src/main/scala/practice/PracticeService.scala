996fa078272fb4eb5cfefd214be6d52c44bed476
hangilc <hangil@chang.jp>
2022-03-11 10:42:44 +0900

    added practice layout

--- practice-app/src/main/scala/practice/PracticeService.scala
+++ practice-app/src/main/scala/practice/PracticeService.scala
@@ -15,13 +15,32 @@ class PracticeMain:
   def ele = ui.ele
 
 class PracticeMainUI:
-  val ele = div(cls := "practice-main")("MAIN")
+  val records = div
+  val ele = div(cls := "practice-main")(
+    header,
+    records(cls := "practice-records")
+  )
+
+  def header: HTMLElement = div(cls := "practice-header")(
+    div("診察", cls := "practice-header-title"),
+    choice(cls := "practice-header-choice"),
+    a("登録薬剤"),
+    a("全文検索")
+  )
+
+  def choice: HTMLElement =
+    PullDown.pullDownLink("患者選択", List(
+      "受付患者選択" -> (() => ()),
+      "患者検索" -> (() => ()),
+      "最近の診察" -> (() => ()),
+      "日付別" -> (() => ())
+    ))
 
 class PracticeRight:
   val ui = new PracticeRightUI
   def ele = ui.ele
 
 class PracticeRightUI:
-  val ele = div(cls := "practice-right-column")("RIGHT")
+  val ele = div(cls := "practice-right-column")
 
 
