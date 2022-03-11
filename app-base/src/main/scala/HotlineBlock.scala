cf0f4743d0d4d04e8cae8aaaa1ea56d02d712553
hangilc <hangil@chang.jp>
2022-03-12 08:56:18 +0900

    revised hotline input

--- app-base/src/main/scala/HotlineBlock.scala
+++ app-base/src/main/scala/HotlineBlock.scala
@@ -10,6 +10,8 @@ import dev.myclinic.scala.model.AppModelEvent
 import scala.util.Success
 import scala.util.Failure
 import org.scalajs.dom.HTMLElement
+import org.scalajs.dom.HTMLInputElement
+import org.scalajs.dom.HTMLTextAreaElement
 
 class HotlineBlock(sendAs: String, sendTo: String)(using fetcher: EventFetcher):
   val ui = new HotlineBlockUI
@@ -39,14 +41,15 @@ class HotlineBlock(sendAs: String, sendTo: String)(using fetcher: EventFetcher):
           handleHotline(event.dataAs[Hotline])
       })
       fetcher.hotlineBeepEventPublisher.subscribe(event => {
-        if event.recipient == sendAs then
-          Api.beep()
+        if event.recipient == sendAs then Api.beep()
       })
 
   private def decodeHotline(data: String): Hotline =
     decode[Hotline](data).toOption.get
+
   private def isHotlineCreatedEvent(model: String, kind: String): Boolean =
     model == Hotline.modelSymbol && kind == AppModelEvent.createdSymbol
+
   private def handleHotline(hotline: Hotline): Unit =
     val recipient = hotline.recipient
     if recipient == sendAs then
@@ -54,6 +57,7 @@ class HotlineBlock(sendAs: String, sendTo: String)(using fetcher: EventFetcher):
       val msg = hotline.message
       val line = s"${rep}> ${msg}\n"
       ui.messages.value += line
+
   private def onSend(msg: String): Unit =
     if !msg.isEmpty then
       val h = Hotline(msg, sendAs, sendTo)
@@ -62,11 +66,30 @@ class HotlineBlock(sendAs: String, sendTo: String)(using fetcher: EventFetcher):
         case Failure(ex) => ShowMessage.showError(ex.getMessage)
       }
   private def regulars: List[(String, () => Unit)] =
-    HotlineEnv.regulars(sendAs).map(m => (m, () => onSend(m)))
+    HotlineEnv
+      .regulars(sendAs)
+      .map(m =>
+        (m, () => HotlineBlock.insertIntoHotlineInput(ui.messageInput, m))
+      )
+
+object HotlineBlock:
+  def insertIntoHotlineInput(hotlineInput: HTMLTextAreaElement, s: String): Unit =
+    val start = hotlineInput.selectionStart
+    val end = hotlineInput.selectionEnd
+    val left = hotlineInput.value.substring(0, start)
+    val right = hotlineInput.value.substring(end)
+    val index = s.indexOf("{}")
+    val msgLeft = if index < 0 then s else s.substring(0, index)
+    val msgRight = if index < 0 then "" else s.substring(index + 2)
+    hotlineInput.value = left + msgLeft + msgRight + right
+    hotlineInput.focus()
+    val pos = start + msgLeft.size
+    hotlineInput.selectionStart = pos
+    hotlineInput.selectionEnd = pos
 
 class HotlineBlockUI:
   import dev.fujiwara.domq.PullDownLink
-  val messageInput = textarea
+  val messageInput: HTMLTextAreaElement = textarea
   val sendButton = button
   val rogerButton = button
   val beepButton = button
