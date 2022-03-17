819386c6b089e7fc298b65cd4ce6a88e4c4f5e99
hangilc <hangil@chang.jp>
2022-03-17 16:26:02 +0900

    added scanner list

--- reception-app/src/main/scala/scan/docscan/ScannerList.scala
+++ reception-app/src/main/scala/scan/docscan/ScannerList.scala
@@ -1,7 +1,18 @@
 package dev.myclinic.scala.web.reception.scan.docscan
 
 import dev.myclinic.scala.model.ScannerDevice
+import dev.myclinic.scala.webclient.{Api, global}
 
 object ScannerList:
+  private var scanners: Option[List[ScannerDevice]] = None
+
   def get(cb: List[ScannerDevice] => Unit): Unit =
-    ???
+    scanners match {
+      case Some(list) => cb(list)
+      case None =>
+        for
+          list <- Api.listScannerDevices()
+        yield
+          scanners = Some(list)
+          cb(list)
+    }
--- reception-app/src/main/scala/scan/docscan/ScannerRow.scala
+++ reception-app/src/main/scala/scan/docscan/ScannerRow.scala
@@ -5,11 +5,13 @@ import dev.myclinic.scala.model.ScannerDevice
 import dev.fujiwara.domq.InPlaceEdit
 import org.scalajs.dom.HTMLAnchorElement
 import org.scalajs.dom.HTMLSpanElement
+import cats.*
+import cats.syntax.all.*
 
 class ScannerRow:
   import ScannerRow.*
   private var dataOpt: Option[ScannerDevice] = None
-  val inPlaceEdit = new InPlaceEdit(new Disp, new Edit, dataOpt)
+  val inPlaceEdit = new InPlaceEdit(new Disp, (new Edit).init, dataOpt)
   val row = new Row
   row.title("スキャナー")
   row.content(inPlaceEdit.ele)
@@ -24,34 +26,37 @@ object ScannerRow:
     val ele = div(disp, editLink)
 
   object Disp:
-    import cats.*
-    import cats.implicits.*
     given ElementProvider[Disp] = _.ele
     given DataAcceptor[Disp, Data] =
-        (DataAcceptor.by(
+        DataAcceptor.by[Disp, Data, HTMLSpanElement, String](
           t => t.disp,
           d => d.map(_.description).getOrElse("（選択されていません）")
-        ): DataAcceptor[Disp, Data]) |+|
+        ) |+|
         DataAcceptor.by(
           t => t.editLink,
           _ match {
-            case Some(_) => "変更"
-            case None    => "選択"
+            case Some(_) => "[変更]"
+            case None    => "[選択]"
           }
         )
     given TriggerProvider[Disp] =
       TriggerProvider.by(_.editLink)
 
   class Edit:
-    var devices: List[ScannerDevice] = List.empty
+    private var devices: List[ScannerDevice] = List.empty
     val select = new Selection[ScannerDevice, ScannerDevice](identity)
     select.formatter = _.description
-    select.set(devices)
     val cancelLink = a
     val ele = div(
       select.ele,
       cancelLink("キャンセル")
     )
+    def init: Edit =
+      ScannerList.get(list => {
+        devices = list
+        select.set(devices)
+      })
+      this
 
   object Edit:
     given ElementProvider[Edit] = _.ele
