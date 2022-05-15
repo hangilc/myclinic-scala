e3a2b07370cb7ff6125b2ce5ead6221daea3c520
hangilc <hangil@chang.jp>
2022-05-15 10:32:51 +0900

    revised gaiyou

--- formatshohousen/shared/src/main/scala/gaiyou/GaiyouDrop.scala
+++ formatshohousen/shared/src/main/scala/gaiyou/GaiyouDrop.scala
@@ -9,31 +9,50 @@ class GaiyouDrop(name: String, amount: String, usage: String) extends Formatter:
   def format(index: Int, ctx: FormatContext): String =
     val (ipre, bpre) = FormatUtil.composePre(index, ctx)
     List(
-      FormatUtil.softSplitLine(ipre, s"$name$zenkakuSpace$amount", ctx.lineSize),
+      FormatUtil.softSplitLine(
+        ipre,
+        s"$name$zenkakuSpace$amount",
+        ctx.lineSize
+      ),
       FormatUtil.softSplitLine(bpre, usage, ctx.lineSize)
     ).mkString("\n")
 
-
 object GaiyouDrop:
   val unit = "(?:ｍＬ|ｇ|ブリスター|瓶|個)"
-  val drugPart = s"($drugNameRegex)$space+($digit+$unit)"
+  val amount = s"$digitOrPeriod+$unit$notSpace*"
+  val drugPart = s"($drugNameRegex)$space+($amount)"
   val drugPattern = drugPart.r
   val usageStart1 = s"１日$digit+回"
   val usageStart2 = s"１回.*$digit+(?:滴|ｇ|噴霧|吸入)"
   val usageStart3 = ".+時"
-  val usageStart = s"(?:$usageStart1|$usageStart2|$usageStart3)"
+  val usageStart4 = s"希釈し(?:、)?$usageStart1"
+  val usageStart = s"(?:$usageStart1|$usageStart2|$usageStart3|$usageStart4)"
   val usage = s"($usageStart(?:.*$notSpace)?)"
   val usagePattern = s"$usage$space*".r
   def tryParse(lead: String, more: List[String]): Option[GaiyouDrop] =
+    tryParseProper(lead, more)
+      .orElse(tryParseExtendingFirstLine(lead, more))
+
+  def tryParseProper(lead: String, more: List[String]): Option[GaiyouDrop] =
     (lead match {
       case drugPattern(name, amount) => Some(name, amount)
-      case _ => None
-    }) .flatMap {
-      case (name, amount) => 
-        more.mkString(zenkakuSpace) match {
-          case usagePattern(usage) => Some(new GaiyouDrop(name, amount, usage))
-          case _ => None
-        }
+      case _                         => None
+    }).flatMap { case (name, amount) =>
+      more.mkString(zenkakuSpace) match {
+        case usagePattern(usage) => Some(new GaiyouDrop(name, amount, usage))
+        case _                   => None
+      }
+    }
+
+  def tryParseExtendingFirstLine(
+      lead: String,
+      more: List[String]
+  ): Option[GaiyouDrop] =
+    val amountPattern = s"$space*$amount$space*".r
+    more match {
+      case (h @ amountPattern) :: t =>
+        tryParseProper(lead + zenkakuSpace + h, t)
+      case _ => None 
     }
 
   val onelineRegex = s"$drugPart$space+$usage"
@@ -41,8 +60,7 @@ object GaiyouDrop:
 
   def tryParseOneLine(lead: String, more: List[String]): Option[GaiyouDrop] =
     (lead :: more).mkString(zenkakuSpace) match {
-      case onelinePattern(name, amount, usage) => Some(new GaiyouDrop(name, amount, usage))
+      case onelinePattern(name, amount, usage) =>
+        Some(new GaiyouDrop(name, amount, usage))
       case _ => None
     }
-    
-
