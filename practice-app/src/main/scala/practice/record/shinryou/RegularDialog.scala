285889851951ee6af5b02c45503fd21a65b54445
hangilc <hangil@chang.jp>
2022-05-28 16:56:01 +0900

    temporary saving

--- practice-app/src/main/scala/practice/record/shinryou/RegularDialog.scala
+++ practice-app/src/main/scala/practice/record/shinryou/RegularDialog.scala
@@ -5,6 +5,9 @@ import dev.myclinic.scala.webclient.{Api, global}
 import java.time.LocalDate
 import dev.myclinic.scala.web.practiceapp.practice.PracticeBus
 import dev.myclinic.scala.model.ShinryouEx
+import dev.myclinic.scala.model.CreateConductRequest
+import dev.myclinic.scala.model.ConductKind
+import dev.myclinic.scala.model.ConductShinryou
 
 class RegularDialog(
     leftNames: List[String],
@@ -25,7 +28,13 @@ class RegularDialog(
     dlog.open()
 
   def onEnter(): Unit =
-    val names: List[String] = panel.selected
+    val (names, kotsuen): (List[String], Option[Boolean]) =
+      panel.selected.foldLeft[(List[String], Boolean)]((List.empty, false)) { case ((n, k), name) =>
+        name match {
+          case "骨塩定量" => (n, true)
+          case _      => (n :+ name, k)
+        }
+      }
     for map <- Api.batchResolveShinryoucodeByName(names, at)
     yield
       val unresolved = (map.collect {
@@ -38,12 +47,22 @@ class RegularDialog(
           masterMap <- Api.batchResolveShinryouMaster(codes, at)
           shinryouList <- Api.batchGetShinryou(shinryouIds)
         yield
-          shinryouList.foreach(s => 
+          shinryouList.foreach(s =>
             val master = masterMap(s.shinryoucode)
             PracticeBus.shinryouEntered.publish(ShinryouEx(s, master))
           )
           dlog.close()
       else
-        val msg = "以下の診療行為のコードを取得できませんでした。\n" + 
+        val msg = "以下の診療行為のコードを取得できませんでした。\n" +
           unresolved.mkString("\n")
         ShowMessage.showError(msg)
+
+  def kotsuenReq: CreateConductRequest =
+    val shinryouName = "骨塩定量ＭＤ法"
+    val kizaiName = "四ツ切"
+    CreateConductRequest(
+      visitId,
+      ConductKind.Gazou.code,
+      Some("骨塩定量に使用")
+    )
+
