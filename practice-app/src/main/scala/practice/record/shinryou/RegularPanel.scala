3210eed8d3a1dec6de372ac239a34b6f10650fe5
hangilc <hangil@chang.jp>
2022-05-29 18:09:05 +0900

    added shinryou menu regular

--- practice-app/src/main/scala/practice/record/shinryou/RegularPanel.scala
+++ practice-app/src/main/scala/practice/record/shinryou/RegularPanel.scala
@@ -8,21 +8,33 @@ case class RegularPanel(
     rightNames: List[String],
     bottomNames: List[String]
 ):
-  val leftChecks: List[CheckLabel[String]] = makeChecks(leftNames)
-  val rightChecks: List[CheckLabel[String]] = makeChecks(rightNames)
-  val bottomChecks: List[CheckLabel[String]] = makeChecks(bottomNames)
-  val checks = leftChecks ++ rightChecks ++ bottomChecks
+  val leftChecks: List[CheckLabel[String] | Unit] = makeChecks(leftNames)
+  val rightChecks: List[CheckLabel[String] | Unit] = makeChecks(rightNames)
+  val bottomChecks: List[CheckLabel[String] | Unit] = makeChecks(bottomNames)
+  val checks = (leftChecks ++ rightChecks ++ bottomChecks).collect {
+    case a if a.isInstanceOf[CheckLabel[String]] => a.asInstanceOf[CheckLabel[String]]
+  }
   val ele = div(
     cls := "practice-shinryou-regular-panel",
-    div(cls := "practice-shinryou-regular-panel-left", leftChecks.map(_.wrap(div))),
-    div(cls := "practice-shinryou-regular-panel-right", rightChecks.map(_.wrap(div))),
-    div(cls := "practice-shinryou-regular-panel-bottom", bottomChecks.map(_.wrap(div)))
+    div(cls := "practice-shinryou-regular-panel-left", leftChecks.map(toElement _)),
+    div(cls := "practice-shinryou-regular-panel-right", rightChecks.map(toElement _)),
+    div(cls := "practice-shinryou-regular-panel-bottom", bottomChecks.map(toElement _))
   )
+
+  def toElement(a: CheckLabel[String] | Unit): HTMLElement = 
+    a match {
+      case () => div(cls := "practice-shinryou-regular-panel-space")
+      case c: CheckLabel[String] => c.wrap(div)
+    }
 
   def selected: List[String] =
     checks.map(_.selected).collect {
       case Some(name) => name
     }
 
-  def makeChecks(names: List[String]): List[CheckLabel[String]] =
-    names.map(name => CheckLabel[String](name, name))
+  def makeChecks(names: List[String]): List[CheckLabel[String] | Unit] =
+    names.map {
+      case "---" => ()
+      case name =>  CheckLabel[String](name, name)
+    }
+
