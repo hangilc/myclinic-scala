ce9d9f0c001920e7f3014d5161bcbf02f8b89af7
hangilc <hangil@chang.jp>
2022-05-31 16:59:59 +0900

    temporary saving

--- practice-app/src/main/scala/practice/record/conduct/ConductMenu.scala
+++ practice-app/src/main/scala/practice/record/conduct/ConductMenu.scala
@@ -2,6 +2,12 @@ package dev.myclinic.scala.web.practiceapp.practice.record.conduct
 
 import dev.fujiwara.domq.all.{*, given}
 import java.time.LocalDate
+import dev.myclinic.scala.web.practiceapp.practice.PracticeBus
+import cats.data.EitherT
+import cats.syntax.all.*
+import scala.concurrent.Future
+import dev.myclinic.scala.webclient.{Api, global}
+import dev.myclinic.scala.model.ConductEx
 
 case class ConductMenu(at: LocalDate, visitId: Int):
   val link = PullDownLink("処置")
@@ -13,7 +19,7 @@ case class ConductMenu(at: LocalDate, visitId: Int):
     List(
       "Ｘ線検査追加" -> (doXp _),
       "注射追加" -> (doConductDrug _),
-      "全部コピー" -> (() => ()),
+      "全部コピー" -> (doCopyAll _),
     )
 
   def doXp(): Unit =
@@ -23,4 +29,14 @@ case class ConductMenu(at: LocalDate, visitId: Int):
   def doConductDrug(): Unit =
     val w = ConductDrugWidget(at, visitId, _.close())
     workarea.prepend(w.ele)
+
+  def doCopyAll(): Unit =
+    for
+      targetVisitId <- EitherT.fromOption[Future](PracticeBus.copyTarget, "コピー先をみつけられませんでした。")
+      srcConductIds <- EitherT.right(Api.listConductForVisit(visitId).map(_.map(_.conductId)))
+      srcConductExList <- EitherT.right(srcConductIds.map(cid => Api.getConductEx(cid)).sequence)
+    yield ???
+
+  def dstConductReq(src: ConductEx): CreateConductReq =
+    
 
