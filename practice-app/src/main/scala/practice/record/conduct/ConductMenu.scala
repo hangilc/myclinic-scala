fd743b6bfb4dde333102762b45d6ceb7dfc9af17
hangilc <hangil@chang.jp>
2022-05-31 17:27:48 +0900

    temporary saving

--- practice-app/src/main/scala/practice/record/conduct/ConductMenu.scala
+++ practice-app/src/main/scala/practice/record/conduct/ConductMenu.scala
@@ -7,7 +7,8 @@ import cats.data.EitherT
 import cats.syntax.all.*
 import scala.concurrent.Future
 import dev.myclinic.scala.webclient.{Api, global}
-import dev.myclinic.scala.model.ConductEx
+import dev.myclinic.scala.model.*
+import dev.myclinic.scala.web.practiceapp.practice.record.shinryou.RequestHelper
 
 case class ConductMenu(at: LocalDate, visitId: Int):
   val link = PullDownLink("処置")
@@ -19,7 +20,7 @@ case class ConductMenu(at: LocalDate, visitId: Int):
     List(
       "Ｘ線検査追加" -> (doXp _),
       "注射追加" -> (doConductDrug _),
-      "全部コピー" -> (doCopyAll _),
+      "全部コピー" -> (doCopyAll _)
     )
 
   def doXp(): Unit =
@@ -32,11 +33,28 @@ case class ConductMenu(at: LocalDate, visitId: Int):
 
   def doCopyAll(): Unit =
     for
-      targetVisitId <- EitherT.fromOption[Future](PracticeBus.copyTarget, "コピー先をみつけられませんでした。")
-      srcConductIds <- EitherT.right(Api.listConductForVisit(visitId).map(_.map(_.conductId)))
-      srcConductExList <- EitherT.right(srcConductIds.map(cid => Api.getConductEx(cid)).sequence)
+      targetVisitId <- EitherT
+        .fromOption[Future](PracticeBus.copyTarget, "コピー先をみつけられませんでした。")
+      srcConductIds <- EitherT.right(
+        Api.listConductForVisit(visitId).map(_.map(_.conductId))
+      )
+      srcConductExList <- EitherT.right(
+        srcConductIds.map(cid => Api.getConductEx(cid)).sequence
+      )
     yield ???
 
-  def dstConductReq(src: ConductEx): CreateConductReq =
-    
-
+  def dstConductReq(src: ConductEx, targetVisitId: Int): CreateConductRequest =
+    def toShinryou(s: ConductShinryouEx): ConductShinryou =
+      ConductShinryou(0, 0, s.shinryoucode)
+    def toDrug(s: ConductDrugEx): ConductDrug =
+      ConductDrug(0, 0, s.iyakuhincode, s.amount)
+    def toKizai(s: ConductKizaiEx): ConductKizai =
+      ConductKizai(0, 0, s.kizaicode, s.amount)
+    CreateConductRequest(
+      targetVisitId,
+      src.kind.code,
+      src.gazouLabel,
+      src.shinryouList.map(toShinryou _),
+      src.drugs.map(toDrug _),
+      src.kizaiList.map(toKizai _)
+    )
