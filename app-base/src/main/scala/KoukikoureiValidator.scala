fca118143290a31b757a9eebf7117f60c68e0a06
hangilc <hangil@chang.jp>
2022-06-21 16:59:28 +0900

    temporary saving

--- app-base/src/main/scala/KoukikoureiValidator.scala
+++ app-base/src/main/scala/KoukikoureiValidator.scala
@@ -14,24 +14,19 @@ import scala.util.{Try, Success, Failure}
 import dev.myclinic.scala.validator.ValidatorUtil.*
 
 object KoukikoureiValidator:
-  sealed trait KoukikoureiError extends ValidationError
+  sealed trait KoukikoureiError(msg: String) extends ValidationError:
+    def message: String = msg
 
-  object NonPositiveKoukikoureiId extends KoukikoureiError:
-    def message: String = "Zero koukikourei-id"
-  object NonPositivePatientId extends KoukikoureiError:
-    def message: String = "Zero patient-id"
-  object NonIntegerHokenshaBangou extends KoukikoureiError:
-    def message: String = "保険者番号が整数でありません。"
-  object NonPositiveHokenshaBangou extends KoukikoureiError:
-    def message: String = "保険者番号が正の整数でありません。"
-  object NonIntegerHihokenshaBangou extends KoukikoureiError:
-    def message: String = "被保険者番号が整数でありません。"
-  object NonPositiveHihokenshaBangou extends KoukikoureiError:
-    def message: String = "被保険者番号が正の整数でありません。"
-  object InvalidFutanWari extends KoukikoureiError:
-    def message: String = "Invalid futan-wari value (should be 1, 2, or 3"
-  object EmptyValidFrom extends KoukikoureiError:
-    def message: String = "期限開始日が入力されていません。"
+  object NonPositiveKoukikoureiId extends KoukikoureiError("Zero koukikourei-id")
+  object NonPositivePatientId extends KoukikoureiError("Zero patient-id")
+  object EmptyHokenshaBangou extends KoukikoureiError("保険者番号が入力されていません。")
+  object NonIntegerHokenshaBangou extends KoukikoureiError("保険者番号が整数でありません。")
+  object NonPositiveHokenshaBangou extends KoukikoureiError("保険者番号が正の整数でありません。")
+  object EmptyHihokenshaBangou extends KoukikoureiError("被保険者番号が入力されていません。")
+  object NonIntegerHihokenshaBangou extends KoukikoureiError("被保険者番号が整数でありません。")
+  object NonPositiveHihokenshaBangou extends KoukikoureiError("被保険者番号が正の整数でありません。")
+  object InvalidFutanWari extends KoukikoureiError("Invalid futan-wari value (should be 1, 2, or 3")
+  object EmptyValidFrom extends KoukikoureiError("期限開始日が入力されていません。")
 
   type Result[T] = Validated[List[KoukikoureiError], T]
 
@@ -42,71 +37,24 @@ object KoukikoureiValidator:
     condValid(patientId > 0, patientId, NonPositivePatientId)
 
   def validateHokenshaBangou(src: String): Result[String] =
-    Try(src.toInt) match {
-      case Success(i) =>
-        if i > 0 then validNec(src)
-        else invalidNec(NonPositiveHokenshaBangou)
-      case Failure(_) => invalidNec(NonIntegerHokenshaBangou)
-    }
+    isNotEmpty(src, EmptyHokenshaBangou)
+      .andThen(toInt(_, NonIntegerHokenshaBangou))
+      .andThen(isPositive(_, NonPositiveHokenshaBangou))
+      .map(_.toString)
 
   def validateHihokenshaBangou(src: String): Result[String] =
-    Try(src.toInt) match {
-      case Success(i) =>
-        if i > 0 then validNec(src)
-        else invalidNec(NonPositiveHihokenshaBangou)
-      case Failure(_) => invalidNec(NonIntegerHihokenshaBangou)
-    }
+    isNotEmpty(src, EmptyHihokenshaBangou)
+      .andThen(toInt(_, NonIntegerHihokenshaBangou))
+      .andThen(isPositive(_, NonPositiveHihokenshaBangou))
+      .map(_.toString)
 
   def validateFutanWari(srcOpt: Option[String]): Result[Int] =
-    val src = srcOpt.getOrElse("")
-    condNec(!(src == null || src.isEmpty), src, InvalidFutanWari)
-      .andThen(str => {
-        Try(str.toInt) match {
-          case Success(i) => validNec(i)
-          case Failure(_) => invalidNec(InvalidFutanWari)
-        }
-      })
-      .andThen {
-        case i @ (1 | 2 | 3) => validNec(i)
-        case _               => invalidNec(InvalidFutanWari)
-      }
+    isSome(srcOpt, InvalidFutanWari)
+      .andThen(toInt(_, InvalidFutanWari))
+      .andThen(i => condValid(i >= 1 && i <= 3, i, InvalidFutanWari))
 
-  def validateValidFrom[E](
-      result: ValidatedNec[E, LocalDate],
-      messageOf: E => String
-  ): Result[LocalDate] =
-    result.fold(
-      err => invalidNec(InvalidValidFrom(err, messageOf)),
-      validNec(_)
-    )
-
-  def validateValidUpto[E](
-      result: ValidatedNec[E, ValidUpto],
-      messageOf: E => String
-  ): Result[ValidUpto] =
-    result.fold(
-      err => invalidNec(InvalidValidUpto(err, messageOf)),
-      validNec(_)
-    )
-
-  def validateKoukikourei(
-      koukikoureiIdResult: Result[Int],
-      patientIdResult: Result[Int],
-      hokenshaBangouResult: Result[String],
-      hihokenshaBangouResult: Result[String],
-      futanWariResult: Result[Int],
-      validFromResult: Result[LocalDate],
-      validUptoResult: Result[ValidUpto]
-  ): Result[Koukikourei] =
-    (
-      koukikoureiIdResult,
-      patientIdResult,
-      hokenshaBangouResult,
-      hihokenshaBangouResult,
-      futanWariResult,
-      validFromResult,
-      validUptoResult
-    ).mapN(Koukikourei.apply)
+  def validateValidFrom(dateOption: Option[LocalDate]): Result[LocalDate] =
+    isSome(dateOption, EmptyValidFrom)
 
   def validateKoukikoureiForEnter(
       patientIdResult: Result[Int],
@@ -116,7 +64,7 @@ object KoukikoureiValidator:
       validFromResult: Result[LocalDate],
       validUptoResult: Result[ValidUpto]
   ): Result[Koukikourei] =
-    validateKoukikourei(
+    (
       validNec(0),
       patientIdResult,
       hokenshaBangouResult,
@@ -124,7 +72,7 @@ object KoukikoureiValidator:
       futanWariResult,
       validFromResult,
       validUptoResult
-    )
+    ).mapN(Koukikourei.apply _)
 
   def validateKoukikoureiForUpdate(
       koukikoureiId: Int,
@@ -135,7 +83,7 @@ object KoukikoureiValidator:
       validFromResult: Result[LocalDate],
       validUptoResult: Result[ValidUpto]
   ): Result[Koukikourei] =
-    validateKoukikourei(
+    (
       validateKoukikoureiIdForUpdate(koukikoureiId),
       patientIdResult,
       hokenshaBangouResult,
@@ -143,4 +91,4 @@ object KoukikoureiValidator:
       futanWariResult,
       validFromResult,
       validUptoResult
-    )
+    ).mapN(Koukikourei.apply _)
