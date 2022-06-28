d74aa0dd18d4b114be2c07d62e45bebb5223e8d2
hangilc <hangil@chang.jp>
2022-06-28 17:39:30 +0900

    rewriting validators

--- validator/src/main/scala/SectionValidator.scala
+++ validator/src/main/scala/SectionValidator.scala
@@ -1,21 +1,43 @@
 package dev.fujiwara.validator.section
 
+import cats.*
+import cats.syntax.all.*
 import cats.data.Validated
 import cats.data.Validated.{Valid, Invalid}
 
-type ValidatedSection[S, T] = Validated[List[(S, String)], T]
+type SectionError[+S] = (S, String)
+type ValidatedSection[+S, +T] = Validated[List[SectionError[S]], T]
 
-trait SectionValidator[S]:
+trait SectionValidator[S](name: String):
   this : S =>
-  def error[T](msg: String): ValidatedSection[S, T] =
+  type Result[T] = ValidatedSection[S, T]
+  def error[T](msg: String): Result[T] =
     Invalid(List((this, msg)))
+
+  def notEmpty(src: String): Result[String] =
+    if src == null || src.isEmpty then error(s"${name}が入力されていません。")
+    else Valid(src)
 
 sealed trait PatientValidator
 
-object PatientIdValidator extends SectionValidator[PatientIdValidator.type] with PatientValidator:
+type Result[T] = ValidatedSection[PatientValidator, T]
 
-  type ValidatedPatientId = ValidatedSection[PatientIdValidator.type, Int]
+object PatientIdValidator extends SectionValidator[PatientIdValidator.type]("patient-id") with PatientValidator:
 
-  def validateForEnter: ValidatedPatientId =
+  def validateForEnter: Result[Int] =
     error("invalid")
+
+object LastNameValidator extends SectionValidator[LastNameValidator.type]("姓") with PatientValidator:
+  def validate(src: String): Result[String] =
+    notEmpty(src)
+
+object Validate:
+  def validate(
+    patientIdResult: ValidatedSection[PatientIdValidator.type, Int],
+    lastNameResult: ValidatedSection[LastNameValidator.type, String]
+  ): ValidatedSection[PatientValidator, (Int, String)] =
+    val gPatientIdREsult: Result[Int] = patientIdResult
+    val gLastNameResult: Result[String] = lastNameResult
+    (gPatientIdREsult, gLastNameResult).mapN((a, b) => (a, b))
+
 
