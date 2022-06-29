136071526dbcc238a71b53fc1c7fcdbd59acafa4
hangilc <hangil@chang.jp>
2022-06-29 22:57:17 +0900

    temporary saving

--- app-base/src/main/scala/KouhiValidator.scala
+++ app-base/src/main/scala/KouhiValidator.scala
@@ -2,6 +2,7 @@ package dev.myclinic.scala.web.appbase.validator
 
 import cats.syntax.all.*
 import cats.data.Validated
+import cats.data.Validated.*
 import dev.fujiwara.validator.section.*
 import dev.fujiwara.validator.section.Implicits.*
 import dev.myclinic.scala.model.ValidUpto
@@ -10,7 +11,9 @@ import dev.myclinic.scala.model.Kouhi
 
 object KouhiValidator:
 
-  sealed trait KouhiError
+  sealed trait KouhiError:
+    def asKouhiError: KouhiError = KouhiError
+  object KouhiError extends KouhiError
 
   object KouhiIdError extends KouhiError
   object FutanshaError extends KouhiError
@@ -20,8 +23,16 @@ object KouhiValidator:
   object PatientIdError extends KouhiError
   object InconsistentValidRangeError extends KouhiError
 
-  extension [E <: KouhiError, T](r: ValidatedResult[E, T])
-    def asKouhiError: ValidatedResult[KouhiError, T] = r
+  // extension [E <: KouhiError, T](r: ValidatedResult[E, T])
+  //   def asKouhiError: ValidatedResult[KouhiError, T] = r
+
+  def asKouhiError[E, T](
+      r: ValidatedResult[E, T]
+  ): ValidatedResult[KouhiError, T] =
+    r match {
+      case Invalid(e) => Invalid(e.map { (e, s) => (KouhiError, s)})
+      case Valid(t)   => Valid(t)
+    }
 
   object KouhiIdValidator extends DatabaseIdValidator(KouhiIdError, "kouhi-id")
 
@@ -50,20 +61,32 @@ object KouhiValidator:
         _.validUpto.value
       )
 
-  def validateForEnter(
+  case class ValidationResults(
       kouhidIdResult: ValidatedResult[KouhiIdError.type, Int],
       futanshaResult: ValidatedResult[FutanshaError.type, Int],
       jukyuushaResult: ValidatedResult[JukyuushaError.type, Int],
       validFromResult: ValidatedResult[ValidFromError.type, LocalDate],
       validUptoResult: ValidatedResult[ValidUptoError.type, ValidUpto],
       patientIdResult: ValidatedResult[PatientIdError.type, Int]
+  )
+
+  type CastToResult[A] = A match {
+    case ValidatedResult[e, t] => ValidatedResult[KouhiError, t]
+  }
+
+  def castToResult[A, E <: KouhiError](a: A): CastToResult[A] = a match {
+    case r: ValidatedResult[E, t] => asKouhiError(r)
+  }
+
+  def validateForEnter(
+      results: ValidationResults
   ): ValidatedResult[KouhiError, Kouhi] =
-    (
-      kouhidIdResult.asKouhiError,
-      futanshaResult.asKouhiError,
-      jukyuushaResult.asKouhiError,
-      validFromResult.asKouhiError,
-      validUptoResult.asKouhiError,
-      patientIdResult.asKouhiError,
-    ).mapN(Kouhi.apply _) |> ConsistentValidRangeValidator.validate
-    
+    val rs = Tuple
+      .fromProductTyped(results)
+      .map[[A] =>> ValidatedResult[KouhiError, A]](
+        [T] => (t: T) => castToResult(t)
+      )
+    ???
+// .mapN(Kouhi.apply _)
+
+// |> ConsistentValidRangeValidator.validate
