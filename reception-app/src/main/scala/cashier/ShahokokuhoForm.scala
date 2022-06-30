23249ed741c9a1f39b53bd0fa797cdf473ae7c9d
hangilc <hangil@chang.jp>
2022-06-30 16:39:52 +0900

    rewrote ShahokokuhoForm

--- reception-app/src/main/scala/cashier/ShahokokuhoForm.scala
+++ reception-app/src/main/scala/cashier/ShahokokuhoForm.scala
@@ -11,6 +11,7 @@ import dev.myclinic.scala.web.appbase.validator.ShahokokuhoValidator
 import dev.myclinic.scala.web.appbase.validator.ShahokokuhoValidator.*
 import dev.myclinic.scala.web.appbase.formprop.Prop
 import dev.myclinic.scala.web.appbase.formprop.Prop.{*, given}
+import dev.fujiwara.validator.section.Implicits.*
 
 case class ShahokokuhoForm(init: Option[Shahokokuho]):
   val props = (
@@ -66,6 +67,13 @@ case class ShahokokuhoForm(init: Option[Shahokokuho]):
     )
   )
 
+  val (hokenshaBangouProp, hihokenshaKigouProp, hihokenshaBangouProp, edabanProp, honninProp, koureiProp, validFromProp, validUptoProp)
+    = props
+
+  val validateProps = (
+    hokenshaBangouProp, hihokenshaKigouProp, hihokenshaBangouProp, honninProp, validFromProp, validUptoProp, koureiProp, edabanProp
+  )
+
   def formatKourei(kourei: Int): String = kourei match {
     case 0 => "高齢でない"
     case 1 => "１割"
@@ -74,96 +82,112 @@ case class ShahokokuhoForm(init: Option[Shahokokuho]):
     case _ => "不明"
   }
 
-  val hokenshaBangouInput = inputText()
-  val hihokenshaKigouInput = inputText()
-  val hihokenshaBangouInput = inputText()
-  val edabanInput = inputText()
-  val honninInput = RadioGroup[Int](
-    List("本人" -> 1, "家族" -> 0),
-    initValue = init.map(_.honninStore)
-  )
-  val koureiInput = RadioGroup[Int](
-    List("高齢でない" -> 0, "１割" -> 1, "２割" -> 2, "３割" -> 3),
-    initValue = init.map(_.koureiStore)
-  )
-  val validFromInput = DateOptionInput(init.map(_.validFrom))
-  val validUptoInput = DateOptionInput(
-    init.flatMap(_.validUpto.value),
-    formatNone = () => "（期限なし）"
-  )(
-    using
-    new InitNoneConverter:
-      def convert: Option[LocalDate] =
-        validFromInput.value.map(_.plusYears(1).minusDays(1))
-  )
-  val dp = DispPanel(form = true)
-  dp.ele(cls := "reception-shahokokuho-form")
-  dp.add(
-    "保険者番号",
-    hokenshaBangouInput(
-      cls := "hokensha-bangou-input",
-      value := initValue(_.hokenshaBangou.toString)
-    )
-  )
-  dp.add(
-    "記号・番号",
-    div(
-      hihokenshaKigouInput(
-        placeholder := "記号",
-        cls := "hihokensha-kigou-input",
-        value := initValue(_.hihokenshaKigou)
-      ),
-      "・",
-      hihokenshaBangouInput(
-        placeholder := "番号",
-        cls := "hihokensha-bangou-input",
-        value := initValue(_.hihokenshaBangou)
-      )
-    )
-  )
-  dp.add("枝番", edabanInput(cls := "edaban"))
-  dp.add("本人・家族", honninInput.ele(cls := "honnin-input"))
-  dp.add("高齢", koureiInput.ele(cls := "kourei-input"))
-  dp.add("期限開始", validFromInput.ele(cls := "valid-from-input"))
-  dp.add("期限終了", validUptoInput.ele(cls := "valid-upto-input"))
-
-  def ele = dp.ele
-
-  def initValue(f: Shahokokuho => String): String =
-    init.map(f).getOrElse("")
-
   def validateForEnter(patientId: Int): Either[String, Shahokokuho] =
-    import ShahokokuhoValidator.*
-    ShahokokuhoValidator
-      .validate(
-        ShahokokuhoIdValidator.validateForEnter,
-        PatientIdValidator.validate(patientId),
-        HokenshaBangouValidator.validateInput(hokenshaBangouInput.value),
-        HihokenshaKigouValidator.validate(hihokenshaKigouInput.value),
-        HihokenshaBangouValidator.validate(hihokenshaBangouInput.value),
-        HonninValidator.validate(honninInput.value),
-        ValidFromValidator.validateOption(validFromInput.value),
-        ValidUptoValidator.validate(validUptoInput.value),
-        KoureiValidator.validate(koureiInput.value),
-        EdabanValidator.validate(edabanInput.value)
-      )
-      .asEither
+    val rs = Prop.resultsOf(validateProps)
+    ShahokokuhoValidator.validate(
+      ShahokokuhoIdValidator.validateForEnter 
+        *: PatientIdValidator.validate(patientId)
+        *: rs).asEither
 
-  def validateForUpdate(patientId: Int): Either[String, Shahokokuho] =
-    import ShahokokuhoValidator.*
-    ShahokokuhoValidator
-      .validate(
-        ShahokokuhoIdValidator.validateOptionForUpdate(
-          init.map(_.shahokokuhoId)
-        ),
-        PatientIdValidator.validate(patientId),
-        HokenshaBangouValidator.validateInput(hokenshaBangouInput.value),
-        HihokenshaKigouValidator.validate(hihokenshaKigouInput.value),
-        HihokenshaBangouValidator.validate(hihokenshaBangouInput.value),
-        HonninValidator.validate(honninInput.value),
-        ValidFromValidator.validateOption(validFromInput.value),
-        ValidUptoValidator.validate(validUptoInput.value),
-        KoureiValidator.validate(koureiInput.value),
-        EdabanValidator.validate(edabanInput.value)
-      )
-      .asEither
+  def validateForUpdate: Either[String, Shahokokuho] =
+    val rs = Prop.resultsOf(validateProps)
+    ShahokokuhoValidator.validate(
+      ShahokokuhoIdValidator.validateOptionForUpdate(init.map(_.shahokokuhoId)) 
+        *: PatientIdValidator.validateOption(init.map(_.patientId))
+        *: rs).asEither
+
+  //   import ShahokokuhoValidator.*
+  //   ShahokokuhoValidator
+  //     .validate(
+  //       ShahokokuhoIdValidator.validateForEnter,
+  //       PatientIdValidator.validate(patientId),
+  //       HokenshaBangouValidator.validateInput(hokenshaBangouInput.value),
+  //       HihokenshaKigouValidator.validate(hihokenshaKigouInput.value),
+  //       HihokenshaBangouValidator.validate(hihokenshaBangouInput.value),
+  //       HonninValidator.validate(honninInput.value),
+  //       ValidFromValidator.validateOption(validFromInput.value),
+  //       ValidUptoValidator.validate(validUptoInput.value),
+  //       KoureiValidator.validate(koureiInput.value),
+  //       EdabanValidator.validate(edabanInput.value)
+  //     )
+  //     .asEither
+
+  // def validateForUpdate(patientId: Int): Either[String, Shahokokuho] =
+  //   import ShahokokuhoValidator.*
+  //   ShahokokuhoValidator
+  //     .validate(
+  //       ShahokokuhoIdValidator.validateOptionForUpdate(
+  //         init.map(_.shahokokuhoId)
+  //       ),
+  //       PatientIdValidator.validate(patientId),
+  //       HokenshaBangouValidator.validateInput(hokenshaBangouInput.value),
+  //       HihokenshaKigouValidator.validate(hihokenshaKigouInput.value),
+  //       HihokenshaBangouValidator.validate(hihokenshaBangouInput.value),
+  //       HonninValidator.validate(honninInput.value),
+  //       ValidFromValidator.validateOption(validFromInput.value),
+  //       ValidUptoValidator.validate(validUptoInput.value),
+  //       KoureiValidator.validate(koureiInput.value),
+  //       EdabanValidator.validate(edabanInput.value)
+  //     )
+  //     .asEither
+
+
+
+  // val hokenshaBangouInput = inputText()
+  // val hihokenshaKigouInput = inputText()
+  // val hihokenshaBangouInput = inputText()
+  // val edabanInput = inputText()
+  // val honninInput = RadioGroup[Int](
+  //   List("本人" -> 1, "家族" -> 0),
+  //   initValue = init.map(_.honninStore)
+  // )
+  // val koureiInput = RadioGroup[Int](
+  //   List("高齢でない" -> 0, "１割" -> 1, "２割" -> 2, "３割" -> 3),
+  //   initValue = init.map(_.koureiStore)
+  // )
+  // val validFromInput = DateOptionInput(init.map(_.validFrom))
+  // val validUptoInput = DateOptionInput(
+  //   init.flatMap(_.validUpto.value),
+  //   formatNone = () => "（期限なし）"
+  // )(
+  //   using
+  //   new InitNoneConverter:
+  //     def convert: Option[LocalDate] =
+  //       validFromInput.value.map(_.plusYears(1).minusDays(1))
+  // )
+  // val dp = DispPanel(form = true)
+  // dp.ele(cls := "reception-shahokokuho-form")
+  // dp.add(
+  //   "保険者番号",
+  //   hokenshaBangouInput(
+  //     cls := "hokensha-bangou-input",
+  //     value := initValue(_.hokenshaBangou.toString)
+  //   )
+  // )
+  // dp.add(
+  //   "記号・番号",
+  //   div(
+  //     hihokenshaKigouInput(
+  //       placeholder := "記号",
+  //       cls := "hihokensha-kigou-input",
+  //       value := initValue(_.hihokenshaKigou)
+  //     ),
+  //     "・",
+  //     hihokenshaBangouInput(
+  //       placeholder := "番号",
+  //       cls := "hihokensha-bangou-input",
+  //       value := initValue(_.hihokenshaBangou)
+  //     )
+  //   )
+  // )
+  // dp.add("枝番", edabanInput(cls := "edaban"))
+  // dp.add("本人・家族", honninInput.ele(cls := "honnin-input"))
+  // dp.add("高齢", koureiInput.ele(cls := "kourei-input"))
+  // dp.add("期限開始", validFromInput.ele(cls := "valid-from-input"))
+  // dp.add("期限終了", validUptoInput.ele(cls := "valid-upto-input"))
+
+  // def ele = dp.ele
+
+  // def initValue(f: Shahokokuho => String): String =
+  //   init.map(f).getOrElse("")
+
