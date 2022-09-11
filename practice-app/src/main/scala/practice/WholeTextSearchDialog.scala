25b54a2ccec2e8dc26acd1a71b223ea10a2789dd
hangilc <hangil@chang.jp>
2022-09-11 16:07:55 +0900

    added search whole text content

--- practice-app/src/main/scala/practice/WholeTextSearchDialog.scala
+++ practice-app/src/main/scala/practice/WholeTextSearchDialog.scala
@@ -6,9 +6,10 @@ import dev.fujiwara.domq.SearchFormPaging
 import dev.myclinic.scala.model.*
 import dev.myclinic.scala.webclient.{Api, global}
 import dev.fujiwara.domq.SearchFormPagingConfig
+import dev.fujiwara.kanjidate.KanjiDate
 
 class WholeTextSearchDialog:
-  import WholeTextSearchDialog.NavUI
+  import WholeTextSearchDialog.{NavUI, Item}
   val dlog = new ModalDialog3()
   given SearchFormPagingConfig with
     override def searchTextInputCssClass = Some("search-text-input")
@@ -22,7 +23,7 @@ class WholeTextSearchDialog:
         (t, v, p, text)
       }
     },
-    (text, visit, patient, searchText) => div(text.content),
+    (text, visit, patient, searchText) => Item(text, visit, patient, searchText).ele,
     new NavUI()
   )
   form.form(button("閉じる", onclick := (() => dlog.close())))
@@ -31,11 +32,23 @@ class WholeTextSearchDialog:
 
   def open(): Unit =
     dlog.open()
+    form.initFocus()
 
 object WholeTextSearchDialog:
   def open(): Unit =
     val dlog = new WholeTextSearchDialog()
     dlog.open()
+
+  case class Item(text: Text, visit: Visit, patient: Patient, searchText: String):
+    val ele = div(
+      div(
+        span(String.format("[%d] %s", patient.patientId, patient.fullName())),
+        span(KanjiDate.dateToKanji(visit.visitedAt.toLocalDate))
+      ),
+      div(
+        text.content
+      )
+    )
 
   case class NavUI( ) extends dev.fujiwara.domq.NavUI:
     val gotoFirstLink = None
