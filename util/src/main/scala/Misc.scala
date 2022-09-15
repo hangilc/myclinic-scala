67134f9ae5af9fc0d46cab918e379aade130b0a9
hangilc <hangil@chang.jp>
2022-09-15 23:07:30 +0900

    added checking digit

--- util/src/main/scala/Misc.scala
+++ util/src/main/scala/Misc.scala
@@ -3,3 +3,39 @@ package dev.myclinic.scala.util
 object Misc:
   def countPages(total: Int, itemsPerPage: Int): Int =
     (total + itemsPerPage - 1) / itemsPerPage
+
+  object HokenUtil:
+    def isValidTodoufukenBangou(bangou: Int): Boolean =
+      bangou >= 1 && bangou <= 47
+
+    def calcCheckingDigit(numberWithoutCheckingDigitArg: Int): Int =
+      var numberWithoutCheckingDigit = numberWithoutCheckingDigitArg
+      if (numberWithoutCheckingDigit < 0) {
+        throw new RuntimeException("Negative number for calcCheckingDigit.")
+      }
+      var s = 0
+      var m = 2
+      while (numberWithoutCheckingDigit > 0) {
+        val d = numberWithoutCheckingDigit % 10
+        var dm = d * m
+        if (dm >= 10) {
+          dm = (dm / 10) + (dm % 10)
+        }
+        s += dm
+        numberWithoutCheckingDigit /= 10
+        if (m == 2) {
+          m = 1
+        } else {
+          m = 2
+        }
+      }
+      s = s % 10
+      var v = 10 - s
+      if (v == 10) {
+        v = 0
+      }
+      v
+
+    def hasValidCheckingDigit(bangou: Int): Boolean =
+      val v = calcCheckingDigit(bangou / 10)
+      v == (bangou % 10)
