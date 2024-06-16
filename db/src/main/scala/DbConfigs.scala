ff2b7eab2e1745b96f7658b874aef78b740001a6
hangilc <hangil@chang.jp>
2024-06-16 16:58:48 +0900

    revised DbConfig

--- db/src/main/scala/DbConfigs.scala
+++ db/src/main/scala/DbConfigs.scala
@@ -4,8 +4,8 @@ import cats.effect.IO
 import io.circe.Json
 
 trait DbConfigs extends Mysql:
-  def getConfig(name: String): IO[Json] = 
-    mysql(DbConfigsPrim.getConfig(name).unique)
+  def getConfig(name: String): IO[Option[Json]] = 
+    mysql(DbConfigsPrim.getConfig(name).option)
 
   def setConfig(name: String, content: Json): IO[Boolean] =
     mysql(DbConfigsPrim.setConfig(name, content))
