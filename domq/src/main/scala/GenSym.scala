d4d8a30310b146800ebbc31f19d67db6294d35db
hangilc <hangil@chang.jp>
2024-11-06 21:49:00 +0900

    removed domq

--- domq/src/main/scala/GenSym.scala
+++
@@ -1,9 +0,0 @@
-package dev.fujiwara.domq
-
-object GenSym:
-  private var serial: Int = 0
-
-  def genSym: String = 
-    serial += 1
-    val id = serial
-    s"domq-gensym-${id}"
--- domq/src/main/scala/prop/ModelUtil.scala
+++
@@ -1,8 +0,0 @@
-package dev.fujiwara.domq.prop
-
-object ModelUtil:
-  def tupleToList[T](tuple: Tuple): List[T] =
-    tuple match {
-      case EmptyTuple => List.empty
-      case h *: t => h.asInstanceOf[T] :: tupleToList[T](t)
-    }
